5c23b6751b85c81756531e4b2d4f09b5113a53a8
Steve Kelly <kd2cca@gmail.com>
2015-06-20 17:07:46 -0500

    add movement control over serial

--- src/index.jl
+++ src/index.jl
@@ -3,12 +3,21 @@ using Markdown
 using Gadfly
 using Distributions
 using Reactive
+using SerialPorts
 
 function main(window)
     push!(window.assets, "layout2")
     push!(window.assets, "icons")
     push!(window.assets, "widgets")
 
+    serial = serialport("/dev/ttyACM0", 250000)
+
+    try
+        serial = open(serial)
+    catch err
+        println("caught error, serial might be already open")
+        println(err)
+    end
 
     desc = md"""
     # Hello, World!
@@ -29,7 +38,7 @@ function main(window)
                       hbox(iconbutton("search"))])
 
     lift(αᵗ, βᵗ, z_axis_inp) do α, β, z_axis
-        vbox(
+        ui = vbox(
             maxwidth(3, t),
             vbox(md"## Dynamic plot",
                 hbox(vbox(
@@ -44,5 +53,9 @@ function main(window)
                   hbox("Z: " |> width(4em), slider(0:0.1:100) >>> z_axis_inp)
                     |> packacross(center)
             ) |> pad(2em))
+        move_cmd = string("G1 X",z_axis," F10000\n")
+        write(serial, move_cmd)
+        @show move_cmd
+        ui
     end
 end
